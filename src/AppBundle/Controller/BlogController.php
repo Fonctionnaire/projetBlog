0cad181ed22330cd4320386800f28f02e220e53d
Fonctionnaire <thibaut.gruffy@gmail.com>
2017-03-19 20:09:15 +0100

    Ajout des templates de la zone admin et leur structure dans le controller. Mise en place de TinyMCE pour l'écriture d'un épisode

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -4,7 +4,10 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Commentaire;
+use AppBundle\Entity\Connexion;
 use AppBundle\Entity\Episode;
+use AppBundle\Form\Type\ConnexionType;
+use AppBundle\Form\Type\EpisodeType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -36,11 +39,74 @@ class BlogController extends Controller
 
     /**
      * @Route("/connexion", name="connexion")
-     * @Method({"POST"})
+     * @Method({"GET","POST"})
      */
-    public function connexionAction()
+    public function connexionAction(Request $request)
     {
-        return $this->render('connexion.html.twig');
+        $connexion = new Connexion();
+        $form = $this->get('form.factory')->create(ConnexionType::class, $connexion);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            $identifiants = $em->getRepository('AppBundle:Connexion')->find(1);
+
+            $log = $identifiants->getLogin();
+            $mdp = $identifiants->getMdp();
+
+            $logSubmit = $form->get('login')->getData();
+            $mdpSubmit = $form->get('mdp')->getData();
+            if($log != $logSubmit || $mdp != $mdpSubmit)
+            {
+                $this->addFlash('error', 'Mauvais login ou mot de passe');
+            }else{
+                return $this->redirectToRoute('admin');
+            }
+        }
+
+        return $this->render('connexion.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @Route("/admin/nouveau-billet", name="nouveauBillet")
+     * @Method({"GET", "POST"})
+     */
+    public function adminAddBilletAction(Request $request)
+    {
+        $newEpisode = new Episode();
+        $form = $this->get('form.factory')->create(EpisodeType::class, $newEpisode);
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $newEpisode->setDate(new \DateTime());
+            $em->persist($newEpisode);
+            $em->flush();
+
+            return $this->redirectToRoute('admin');
+        }
+
+
+        return $this->render('adminAddBillet.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @Route("/admin/update/{id}", name="updateBillet")
+     * @Method({"GET", "POST"})
+     */
+    public function adminUpdateDeleteBilletAction()
+    {
+
+        return $this->render('adminUpdateDelete.html.twig');
+    }
+
+    /**
+     * @Route("/admin/commentaire/{id}", name="moderationComment")
+     * @Method({"GET", "POST"})
+     */
+    public function moderationCommentsAction()
+    {
+        return $this->render('adminComments.html.twig');
     }
 
     /**
